6e66216c09b6cc11655df94b850599a13e360fd9
Sergey <tsvetkoffgolod@yandex.ru>
2021-08-11 19:32:12 +0300

    Code Progress JS

--- scritp.js
+++ scritp.js
@@ -2,14 +2,38 @@ let content = document.querySelector('.content');
 let profile = content.querySelector('.profile');
 let editButton = content.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+let closePopup = document.querySelector('.popup__close-button');
 
 
-function editProf() {
-  console.log('ok');
+function openProfile() {
   popup.classList.add('popup_opened');
   popup.classList.remove('popup');
 };
 
-// editButton.hasEventListener('click');
-editButton.addEventListener('click', editProf);
-// editProf();
+function closeProfile() {
+  popup.classList.add('popup');
+  popup.classList.remove('popup_opened');
+};
+
+editButton.addEventListener('click', openProfile);
+closePopup.addEventListener('click', closeProfile);
+
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.popup__form-name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__form-job'); // Воспользуйтесь инструментом .querySelector()
+let profileName = document.querySelector('.profile__name');
+let profileJobe = document.querySelector('.profile__job');
+
+
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJobe.textContent = jobInput.value;
+    closeProfile();
+};
+formElement.addEventListener('submit', formSubmitHandler);
