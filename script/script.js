a3635fb606fd38e2e041bc9370754c8814bf3022
Sergey <tsvetkoffgolod@yandex.ru>
2021-08-27 22:43:06 +0300

    JS progress

--- script/script.js
+++ script/script.js
@@ -7,14 +7,18 @@ let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJobe = document.querySelector('.profile__job');
+let buttonAdd = document.querySelector('.profile__add-button');
+
+
+
 
 function openEditPopup() {
-  nameInput.value = profileName.textContent; //
-  jobInput.value = profileJobe.textContent; // Если я правильно понял то так, по крайней мере так показывает)
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJobe.textContent;
   popup.classList.add('popup_opened');
 };
 
-function closeEditPopup() {   // имя closePopup нельзя использовать тк оно задейственно в переменной, поэтому closeEditPopup
+function closeEditPopup() {
   popup.classList.remove('popup_opened');
 };
 
@@ -25,7 +29,52 @@ function formSubmitHandler (evt) {
     closeEditPopup();
 };
 
+// -----------------------------------------------------------------------------------------
+let cardPopup = document.querySelector('#popup-card');
+let closeAddButton = cardPopup.querySelector('.popup__close-button')
+
+let element = document.querySelector('.elements')
+
+let cardTemplate = document.querySelector('#cardBlock').content;
+let cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+// let cardElement = cardTemplate.cloneNode(true);
+let cardImage = cardElement.querySelector('.element__image');
+let cardTitle = cardElement.querySelector('.element__title');
+let cardInputImage = cardPopup.querySelector('.element__input_type_img');
+let cardInputTitle = cardPopup.querySelector('.element__input_type_title');
+
+let cardFormPopup = cardPopup.querySelector('.popup__form');
+
+
+
+function newCard() {
+  cardPopup.classList.add('popup_opened');
+}
+
+function closeAddPopup () {
+  cardPopup.classList.remove('popup_opened')
+};
+
+function cardSubmitHandler (evt) {
+  evt.preventDefault();
+
+  cardImage.src = cardInputImage.value;
+  cardTitle.textContent = cardInputTitle.value;
+
+  element.prepend(cardElement);
+
+
+  closeAddPopup();
+
+};
+
+
+
 
 editButton.addEventListener('click', openEditPopup);
 closePopup.addEventListener('click', closeEditPopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
+buttonAdd.addEventListener('click', newCard);
+closeAddButton.addEventListener('click' , closeAddPopup);
+cardFormPopup.addEventListener('submit', cardSubmitHandler);
